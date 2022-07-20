6d8757a5cef8ba050e27d31d386e23113abf0df8
will <cxydiary@sina.com>
2022-07-20 12:47:32 +0800

    修改

--- suffix.sh
+++ suffix.sh
@@ -3,11 +3,14 @@
 # Script to compile and create files for each openwrt
 # --------------------------------------------------------
 #1. 修改默认IP
-sed -i 's/192.168.1.1/10.10.10.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+
 
 #2. 修改自定义固件名,增加编译日期
 #sed -i "s/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)=\"IMG_PREFIX:=$(shell date +%m-%d)-$(VERSION_DIST_SANITIZED)'\"/g" include/image.mk
 
+#3. 修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
